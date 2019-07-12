f5f30af5eda0ab74269a5b514ff93359f8582e55
mlagutko <mlagutko@decisiv.net>
2019-07-12 14:35:09 +0300

    Added Engine model

---
+++ engine/src/main/java/com/jobsflow/engine/Application.java
@@ -0,0 +1,15 @@
+package main.java.com.jobsflow.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories("com.jobsflow.data.*")
+@EntityScan("com.jobsflow.data.*")
+@SpringBootApplication(scanBasePackages={"com.jobsflow.data.*"})
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
